5e8b3bc6fd52b86a2d16f53f1fccd8c66c339b7a
Gabriel Rosario <gabriel.rosario2@upr.edu>
2018-11-05 23:14:16 -0500

    lexer fixed

--- lexer.py
+++ lexer.py
@@ -1,41 +1,17 @@
 import ply.lex as lex
 
-# Reserved Words
-# reserved = {
-#     'new_project': 'NEW_PROJECT', #Done
-#     'add_member': 'ADD_MEMBER', #Done
-#     'delete_member' : 'DELETE_MEMBER', #Done
-#     'view_members' : 'VIEW_MEMBERS', #Done
-#     'create_brainstorm':'CREATE_BRAINSTORM', #Done
-#     'add_idea':'ADD_IDEA', #Done
-#     'add_task': 'ADD_TASK', #Done
-#     'edit_task': 'EDIT_TASK', #Done
-#     'delete_task': 'DELETE_TASK', #Done
-#     'completed_task': 'COMPLETED_TASK', #Done
-#     'assign_task': 'ASSIGN_TASK', #Done
-#     'view_brainstorm':'VIEW_BRAINSTORM', #Done
-#     'view_schedule': 'VIEW_SCHEDULE', #Done
-#     'view_tasks' : 'VIEW_TASKS', #Done
-#     'list_today' : 'LIST_TODAY', #Done
-#     'list_week' : 'LIST_WEEK', #Done
-#     'list_overdue' : 'LIST_OVERDUE', #Done
-#     'generate_project' : 'GENERATE_PROJECT', #Done
-#     'generate_pie' : 'GENERATE_PIE',
-#     'generate_bar' : 'GENERATE_BAR',
-#     'generate_line' : 'GENERATE_LINE'
-# }
-
-reserved = {
+#list of reserved words that can't be used for variable names, etc.
+reserved_words = {
     'define': 'DEFINE',
     'as': 'AS',
     'addWatermark': 'ADD_WATERMARK',
 }
 
 #Tokens
-tokens = ["NAME","NUMBER","PHRASE","NUMBERLIST","NAMELIST",'LPAREN', 'RPAREN', 'COMMA',"ID","FILE"] + list(reserved.values())
+tokens = ["NAME","NUMBER","PHRASE","NUMBERLIST","NAMELIST",'LPAREN', 'RPAREN', 'COMMA',"ID","FILE"] + list(reserved_words.values())
 
 
-# Simple Regular Expressions
+# RegEx
 t_ignore = ' \t'
 t_NAME = r'[a-zA-Z]+'
 t_PHRASE = r'[a-zA-Z ]+'
@@ -43,7 +19,6 @@ t_NUMBER = r'[\d+]+'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_COMMA = r'\,'
-## NEED regular expression to accept characters and spaces - for tasks
 
 # Define a rule for reserved words
 def t_ID(t):
@@ -56,7 +31,7 @@ def t_LIST(t):
     r'[-?0-9,]+[-?0-9]'
     t.type = 'NUMBERLIST'
     return t
-    
+
 # Regular expression for list of words
 def t_LIST2(t):
     r'[\'a-zA-z\']+[,\'a-zA-Z\']*'
