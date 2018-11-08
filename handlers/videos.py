06c63906d344023d5f59b90c6f279ce59a2f9978
casualguy <xxxrockerguy@gmail.com>
2018-11-08 13:40:06 -0400

    Videos updated

--- handlers/videos.py
+++ handlers/videos.py
@@ -4,7 +4,6 @@ from send2trash import send2trash
 from subprocess import Popen, PIPE
 from ffmpy import FFmpeg, FFprobe
 
-
 class Video:
     __tags = ['title', 'artist', 'album', 'track', 'genre', 'date', 'description', 'comment', 'grouping']
 
@@ -14,6 +13,8 @@ class Video:
         self.filename, self.extension = ntpath.basename(pathname).rsplit('.', 1)
         self.directory = ntpath.dirname(pathname)
         self.metadata = {}
+        self.subtitles = {}
+        self.artwork = None
 
     # def load(self, pathname):
     #     assert ntpath.isfile(pathname), "given pathname doest not belong to a file"
@@ -35,20 +36,58 @@ class Video:
         assert tag in self.__tags, "tag is not supported"
         self.metadata[tag] = data
 
+    def add_subs(self, sub_path, lang):
+        self.subtitles[lang] = sub_path
+
+    def set_artwork(self, img_path):
+        self.artwork = img_path
+
+    def rename(self, name):
+        self.filename = name
+
     def save_tags(self):
+        # Setting file output name
         signature = "[MyTag]"
         out_name = signature + self.filename + '.' + self.extension
         output = os.path.join(self.directory, out_name)
+
+        # Setting Inputs
+        inputs = {self.pathname: ['-v', 'error']}
+
+        # ffmpeg parameters
         param = []
+        # metadata tags
         for tag in self.metadata:
             param.append('-metadata')
             param.append('{}={}'.format(tag, self.metadata[tag]))
+        # artwork
+        if self.artwork:
+            art_param = ['-map', '1', '-map', '0', '-disposition:0', 'attached_pic']
+            param += art_param
+            inputs[self.artwork] = None
         param.append('-c')
         param.append('copy')
+
+        # subs
+        if self.subtitles:
+            map = 2 if self.artwork else 1  # if there is artwork subtitles will begin at input 2 else 1
+            sub_param = []
+            for sub in self.subtitles:
+                inputs[self.subtitles[sub]] = None
+                sub_param.append('-map',)
+                sub_param.append(str(map))
+                map += 1
+            sub_param.append('-c:s')
+            sub_param.append('mov_text')
+            param += sub_param
+
+
         ff = FFmpeg(
-            inputs={self.pathname: ['-v', 'error']},
+            # self.pathname: ['-v', 'error'], self.artwork: None
+            inputs=inputs,
             outputs={output: param}
         )
+        print(ff.cmd)
         ff.run()
         send2trash(self.pathname)
         new_path = output.replace(signature, '')
@@ -65,17 +104,14 @@ class Video:
     #     )
     #     print(ff.cmd)
 
-    def rename(self, name):
-        self.filename = name
-
-    def add_artwork(self, img):
-        # output = self.directory+'/'+self.filename+".mytag."+self.extension
-        output = "{}/{}.mytag.{}".format(self.directory, self.filename, self.extension)
-        ff = FFmpeg(
-            inputs={self.pathname: ['-v', 'error'], img: None},
-            outputs={output: ['-map', '1', '-map', '0', '-c', 'copy', '-disposition:0', 'attached_pic']}
-        )
-        print(ff.cmd)
+    # def add_artwork(self, img):
+    #     # output = self.directory+'/'+self.filename+".mytag."+self.extension
+    #     output = "{}/{}.mytag.{}".format(self.directory, self.filename, self.extension)
+    #     ff = FFmpeg(
+    #         inputs={self.pathname: ['-v', 'error'], img: None},
+    #         outputs={output: ['-map', '1', '-map', '0', '-c', 'copy', '-disposition:0', 'attached_pic']}
+    #     )
+    #     print(ff.cmd)
 
     def add_subtitle(self, subs, language):
         output = "{}/{}.mytag.{}".format(self.directory, self.filename, self.extension)
